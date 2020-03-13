9b5f89800a4580625b0744008852d5e710af3bfd
Alabs <alabson.inc@gmail.com>
2020-03-13 09:41:09 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.modal').modal(); $('.parallax').parallax(); $('.tooltipped').tooltip(); $('.tap-target').tapTarget(); $('.materialboxed').materialbox(); $('.dropdown-trigger').dropdown(); $('.datepicker').datepicker({
+        disableWeekends: true,
+    });
+})
